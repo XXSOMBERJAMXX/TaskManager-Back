b473526a186e9d3fd0cd0a10c585e6f1a3f73a6e
Diego Morales <129819167+XXSOMBERJAMXX@users.noreply.github.com>
2025-03-07 15:21:13 -0600

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 
 // Conectar a MongoDB
 mongoose.connect(process.env.MONGO_URI, {
